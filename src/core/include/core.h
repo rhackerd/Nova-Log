ea8d5d874d51373f3524c6bcfa4ecabb7f27a687
rhacker <103863606+rhackerd@users.noreply.github.com>
2025-07-09 07:58:58 +0200

    Initial commit

---
+++ src/core/include/core.h
@@ -0,0 +1,17 @@
+#ifndef CORE_HPP
+#define CORE_HPP
+
+#include "module.h"
+
+class Core {
+    private:
+
+    public:
+        Core() {};
+        ~Core() {};
+
+        void load() {};
+        void unload() {};
+};
+
+#endif
